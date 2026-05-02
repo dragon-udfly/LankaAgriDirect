a5c0c0e6e59adcf2ad49d0a35cf884ed04200b68
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-05-02 21:06:01 +0530

    Update LankaAgriDirectApplication.java
    Updated main run file to show added application variable

--- src/main/java/com/example/lankaagridirect/LankaAgriDirectApplication.java
+++ src/main/java/com/example/lankaagridirect/LankaAgriDirectApplication.java
@@ -1,7 +1,9 @@
 package com.example.lankaagridirect;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,9 +12,15 @@ public class LankaAgriDirectApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(LankaAgriDirectApplication.class);
 
+    @Value("${app.system.name}")
+    private String systemName;
+
     public static void main(String[] args) {
         SpringApplication.run(LankaAgriDirectApplication.class, args);
-        logger.info("LankaAgriDirectApplication started successfully!");
     }
 
+    @PostConstruct
+    public void logStatus() {
+        logger.info("{} started successfully!", systemName);
+    }
 }
