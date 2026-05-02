07b186980bb45ed5aff031362ce1f9d60521ecaa
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-05-02 20:28:24 +0530

    Update LankaAgriDirectApplication.java
    Added log message

--- src/main/java/com/example/lankaagridirect/LankaAgriDirectApplication.java
+++ src/main/java/com/example/lankaagridirect/LankaAgriDirectApplication.java
@@ -1,13 +1,18 @@
 package com.example.lankaagridirect;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LankaAgriDirectApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(LankaAgriDirectApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(LankaAgriDirectApplication.class, args);
+        logger.info("LankaAgriDirectApplication started successfully!");
     }
 
 }
