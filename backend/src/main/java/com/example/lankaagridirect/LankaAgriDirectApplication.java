11b7c3d50c8219086b90ce2eeb1e8d1f9e78429a
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-05-03 13:25:47 +0530

    Project Reinitialized including web, mobile and backend parts.
    Monorepo for backend, and frontend

---
+++ backend/src/main/java/com/example/lankaagridirect/LankaAgriDirectApplication.java
@@ -0,0 +1,13 @@
+package com.example.lankaagridirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LankaAgriDirectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LankaAgriDirectApplication.class, args);
+    }
+
+}
