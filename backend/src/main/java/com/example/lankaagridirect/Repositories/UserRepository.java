3d5c1f63e3ff0767b0e2c37a5d53b47e1c3e8518
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-05-06 23:17:33 +0530

    Added API plan

--- backend/src/main/java/com/example/lankaagridirect/Repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.lankaagridirect.Repositories;
-
-import com.example.lankaagridirect.Models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    // Custom query methods can go here if needed
-}
