c752f70684c81795a34898044a24aa308cc7579b
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-05-19 20:55:04 +0530

    refactor: remove unnecessary React import in NewRegistrationsPage

--- admin-web/src/pages/NewRegistrationsPage.tsx
+++ admin-web/src/pages/NewRegistrationsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getProducers, getProducerById } from '../api/adminApi';
 import '../styles/NewRegistrations.css';
 
